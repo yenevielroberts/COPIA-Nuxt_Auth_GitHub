80e29131ab77d1e8f2127685b4171f358e0fda8f
Yeneviel Roberts <yeneviel.roberts.7e8@itb.cat>
2026-02-24 17:45:50 +0100

    inicio practica: Activitat 4: Evolució a Full-Stack amb Nuxt

--- server/db/schema.ts
+++ server/db/schema.ts
@@ -1,3 +1,4 @@
+import { float } from "drizzle-orm/mysql-core";
 import { sqliteTable, text, integer } from "drizzle-orm/sqlite-core"
 
 export const users = sqliteTable("users", {
@@ -7,3 +8,29 @@ export const users = sqliteTable("users", {
     login: text("login"),
     password: text("password"),
 });
+
+export const galaxias=sqliteTable ("galaxias",{
+  id: integer("id").primaryKey(),
+  nombre: text("name"),
+  num_planetas:integer("num_planetas"),
+  curiosidades: text("curiosidades"),
+  tipo: integer("tipo"),
+
+})
+
+export const planetas=sqliteTable ("planetas",{
+  id: integer("id").primaryKey(),
+  nombre: text("name"),
+  anillos:integer("anillos"),
+  satelites: integer("anillos"),
+  habitabilidad: text("habitabilidad"),
+  orbita_dias: integer("orbita_dias"),
+  galaxia_id: integer("galaxia_id").references(()=>galaxias.id),
+
+})
+
+export const planetas_users =sqliteTable ("planetas_users",{
+      id: integer("id").primaryKey(),
+      id_user: integer ("user_id").references(()=>users.id),
+      id_galaxias: integer ("id_galaxias").references(()=>galaxias.id),
+})
