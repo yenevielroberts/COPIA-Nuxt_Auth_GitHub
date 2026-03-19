dcd3b16511a611135e65c0ad3007c1bc108d7a9b
Yeneviel <yeneviel.roberts.7e8@itb.cat>
2026-03-19 17:42:44 +0100

    cors

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -7,6 +7,19 @@ export default defineNuxtConfig({
   devtools: { enabled: true },
   //css:["tailwindcss","@nuxt/ui"],
   modules: ["@nuxt/eslint", "@nuxt/ui", "nuxt-auth-utils"],
+nitro: {
+  routeRules: {
+    '/**': {// Afegeix les capçaleres CORS a totes les rutes
+      cors: true,
+      headers: {
+        'Access-Control-Allow-Origin': 'http://localhost:9000',
+        'Access-Control-Allow-Credentials': 'true',
+        'Access-Control-Allow-Methods': 'GET, POST, PUT, DELETE, OPTIONS',
+        'Access-Control-Allow-Headers': 'Content-Type, Authorization'
+      }
+    }
+  }
+},
   runtimeConfig: {
     oauth: {
       // provider in lowercase (github, google, etc.)
@@ -17,5 +30,6 @@ export default defineNuxtConfig({
         clientSecret: ""
       }
     }
-  }
+  },
+
 });
