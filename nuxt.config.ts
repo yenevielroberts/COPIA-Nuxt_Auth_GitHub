f52b29dfb80c3170c04cfd7f91667d1fa62bccde
Yeneviel <yeneviel.roberts.7e8@itb.cat>
2026-03-23 20:01:49 +0100

    cors

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -14,8 +14,8 @@ nitro: {
       headers: {
         'Access-Control-Allow-Origin': 'http://localhost:9000',
         'Access-Control-Allow-Credentials': 'true',
-        'Access-Control-Allow-Methods': 'GET, POST, PUT, DELETE, OPTIONS',
-        'Access-Control-Allow-Headers': 'Content-Type, Authorization'
+        'Access-Control-Allow-Methods': '*',
+        'Access-Control-Allow-Headers': '*'
       }
     }
   }
